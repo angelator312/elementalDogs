3887a70b61a9abc6505ef37ac2fecb2c8c82b8ee
angelator312 <angelator312@gmail.com>
2025-03-04 22:57:57 +0200

    Update cards_player

--- scripts/cards_player.gd
+++ scripts/cards_player.gd
@@ -4,24 +4,52 @@ extends Node2D
 
 @export var width=720
 var mouse_change=0
+var mouse_in:=false
+var golNaPilnataCarta=(width/720)*180
+var nomPilna:=0
 func positionFromI(i:int)->Vector2:
-	return Vector2(i*GlobalConfig.get_logo_width()+(width/720)*180,GlobalConfig.get_height_of_card()/2)
-func cardFromX(x:float):
-	return (x-(width/720)*180)/GlobalConfig.get_logo_width()
+	return Vector2(i*GlobalConfig.get_logo_width()+golNaPilnataCarta,GlobalConfig.get_height_of_card()/2)
+func cardFromX(x:float,nomPilna:int):
+	var malkiFirstChast=nomPilna*GlobalConfig.get_logo_width()
+	var goleminaChast=malkiFirstChast+golNaPilnataCarta
+	if x>goleminaChast:
+		var cardIndex=(x-goleminaChast)/GlobalConfig.get_logo_width()
+		return cardIndex
+	elif x>malkiFirstChast:
+		return nomPilna
+	else:
+		return x/GlobalConfig.get_logo_width()
 func mouse_entered():
-	var card=cardFromX(get_local_mouse_position().x)
-	print(card)
-
-func on_start():
-	$Area2D.mouse_entered.connect(mouse_entered)
+	mouse_in=true
+func mouse_exit():
+	mouse_in=false
+	var sz=CardDecks.hands[nom].size()
+	nomPilna=sz-1;
+	drawCards(nomPilna)
+func makeCards():
 	var sz=CardDecks.hands[nom].size()
 	for i in range(0,sz):
 		var cardName=CardDecks.hands[nom].getCard(i)
 		var card=CardDecks.makeAdvancedCard(cardName,i)
+		if !get_node_or_null(str(i)):
+			card.name=str(i)
+			self.add_child(card)
+func drawCards(pilnaCarta:=0):
+	var sz=CardDecks.hands[nom].size()
+	for i in range(0,pilnaCarta+1):
+		var card=get_node(str(i))
 		card.position=positionFromI(i)
-		card.name=str(i)
-		self.add_child(card)
+	for i in range(pilnaCarta+1,sz):
+		var card=get_node(str(i))
+		card.position=positionFromI(i)+Vector2(golNaPilnataCarta,0)
+func on_start():
+	$Area2D.mouse_entered.connect(mouse_entered)
+	$Area2D.mouse_exited.connect(mouse_exit)
+	var sz=CardDecks.hands[nom].size()
+	makeCards()
+	drawCards(sz-1)
 	create_polygon(sz-1)
+	nomPilna=sz-1
 
 func end_turn():
 #	tegli animatcia
@@ -31,11 +59,15 @@ func end_turn():
 	card.position=positionFromI(sz-1)+Vector2(mouse_change,0)
 	self.add_child(card)
 	create_polygon(sz-1)
+	if nomPilna==sz-2:nomPilna=sz-1
+	makeCards()
+	drawCards(nomPilna)
 func create_polygon(brCard,raztegnata:=0):
 	collision_shape_2d.shape.size=positionFromI(brCard)+Vector2(GlobalConfig.get_width_of_card(),0)
 	collision_shape_2d.position=Vector2(collision_shape_2d.shape.size.x/2+mouse_change,GlobalConfig.get_height_of_card()/2)
 	print(collision_shape_2d.shape.size)
 	#TODO:Working create polygon and conecting a signals that call the coresponding card's mouse_enter() and mouse_excited()
+
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_WHEEL_UP:
@@ -44,3 +76,8 @@ func _unhandled_input(event: InputEvent) -> void:
 		if event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
 			collision_shape_2d.position.x-=GlobalConfig.get_change_on_up_mouse_wheel()
 			mouse_change-=GlobalConfig.get_change_on_up_mouse_wheel()
+	if mouse_in:
+		if event is InputEventMouseMotion:
+			var card=cardFromX(get_local_mouse_position().x,nomPilna)
+			nomPilna=card
+			drawCards(nomPilna)
